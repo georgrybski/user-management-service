382bed0505b496dc70da7edb6a309900803c0ba7
georgrybski <rybskigeorg@gmail.com>
2023-04-09 10:56:50 -0300

    Refactored UserMapper

--- src/main/java/com/usermanagementservice/mapper/UserMapper.java
+++ src/main/java/com/usermanagementservice/mapper/UserMapper.java
@@ -5,19 +5,18 @@ import com.usermanagementservice.dto.user.UserRegistrationDTO;
 import com.usermanagementservice.dto.user.UserUpdateDTO;
 import com.usermanagementservice.mapper.config.MappingConfig;
 import com.usermanagementservice.model.User;
+import lombok.AllArgsConstructor;
 import org.mapstruct.Mapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
+@AllArgsConstructor
 @Mapper(config = MappingConfig.class)
 public abstract class UserMapper {
 
-    @Autowired
     AddressMapper addressMapper;
 
     public abstract UserDTO toDTO(User user);
-
     public abstract User toEntity(UserDTO userDTO);
     public abstract User toEntity(UserUpdateDTO userUpdateDTO);
     public abstract User toEntity(UserRegistrationDTO userRegistrationDTO);
