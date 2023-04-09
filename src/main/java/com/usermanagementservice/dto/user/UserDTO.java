b8dc0381be998fc44382ba9dc608eb2decc9257b
georgrybski <rybskigeorg@gmail.com>
2023-04-09 13:17:19 -0300

    Refactored UserDTO

--- src/main/java/com/usermanagementservice/dto/user/UserDTO.java
+++ src/main/java/com/usermanagementservice/dto/user/UserDTO.java
@@ -1,46 +1,16 @@
 package com.usermanagementservice.dto.user;
 
 import com.usermanagementservice.dto.address.AddressDTO;
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.List;
 
-@Data
+@Getter
+@Setter
 public class UserDTO {
     private Long id;
     private String name;
     private String email;
     private List<AddressDTO> addresses;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public List<AddressDTO> getAddresses() {
-        return addresses;
-    }
-
-    public void setAddresses(List<AddressDTO> addresses) {
-        this.addresses = addresses;
-    }
 }
