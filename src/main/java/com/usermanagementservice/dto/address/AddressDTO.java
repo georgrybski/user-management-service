1b717cbc384db5a610aca39ff272e763d351ff55
georgrybski <rybskigeorg@gmail.com>
2023-04-09 11:51:23 -0300

    Refactored AddressDTO

--- src/main/java/com/usermanagementservice/dto/address/AddressDTO.java
+++ src/main/java/com/usermanagementservice/dto/address/AddressDTO.java
@@ -1,8 +1,10 @@
 package com.usermanagementservice.dto.address;
 
-import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
-@Data
+@Getter
+@Setter
 public class AddressDTO {
     private Long id;
     private String fullName;
@@ -12,68 +14,4 @@ public class AddressDTO {
     private String city;
     private String country;
     private String zipCodeOrPostalCodeOrCEP;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
-
-    public void setPhoneNumber(String phoneNumber) {
-        this.phoneNumber = phoneNumber;
-    }
-
-    public String getStreet() {
-        return street;
-    }
-
-    public void setStreet(String street) {
-        this.street = street;
-    }
-
-    public String getNumber() {
-        return number;
-    }
-
-    public void setNumber(String number) {
-        this.number = number;
-    }
-
-    public String getCity() {
-        return city;
-    }
-
-    public void setCity(String city) {
-        this.city = city;
-    }
-
-    public String getCountry() {
-        return country;
-    }
-
-    public void setCountry(String country) {
-        this.country = country;
-    }
-
-    public String getZipCodeOrPostalCodeOrCEP() {
-        return zipCodeOrPostalCodeOrCEP;
-    }
-
-    public void setZipCodeOrPostalCodeOrCEP(String zipCodeOrPostalCodeOrCEP) {
-        this.zipCodeOrPostalCodeOrCEP = zipCodeOrPostalCodeOrCEP;
-    }
 }
