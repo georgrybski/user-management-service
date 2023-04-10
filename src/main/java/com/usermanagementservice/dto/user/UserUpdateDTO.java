a6d3d71fe042929f2ab07d244cff165a50f215fe
georgrybski <rybskigeorg@gmail.com>
2023-04-10 03:58:42 -0300

    Refactored UserUpdateDTO

--- src/main/java/com/usermanagementservice/dto/user/UserUpdateDTO.java
+++ src/main/java/com/usermanagementservice/dto/user/UserUpdateDTO.java
@@ -1,10 +1,14 @@
 package com.usermanagementservice.dto.user;
 
 import com.usermanagementservice.dto.address.AddressRegistrationDTO;
+import lombok.Getter;
+import lombok.Setter;
 
-import java.util.ArrayList;
+
 import java.util.List;
 
+@Getter
+@Setter
 public class UserUpdateDTO {
     private String name;
 
@@ -13,36 +17,4 @@ public class UserUpdateDTO {
     private String password;
 
     private List<AddressRegistrationDTO> addresses;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public List<AddressRegistrationDTO> getAddresses() {
-        return addresses;
-    }
-
-    public void setAddresses(List<AddressRegistrationDTO> addresses) {
-        this.addresses = addresses;
-    }
 }
